597ef4debb24481e2d0d488f7e40d0e9f9f2a3fc
thesimplekid <thesimplekid@brendanmurphy.xyz>
2020-10-14 18:39:58 -0400

    retunes al

--- src/temperatures/temperatures.controller.ts
+++ src/temperatures/temperatures.controller.ts
@@ -1,11 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
+import  { TemperaturesService }  from './temperatures.service';
 
 @Controller('temperatures')
 export class TemperaturesController {
+  constructor(private  temperaturesService: TemperaturesService){}
   @Get()
-  async getReadings() {
+  async getReading() {
     console.log("got request")
     return { "hi": 'hello world' }
+  }
+
+  @Get('/all')
+  async getAllReading() {
+    console.log('all')
+    return this.temperaturesService.getTemps();
   }
 
 
